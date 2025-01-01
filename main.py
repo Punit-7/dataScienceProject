c8686d206cd78e99c8a06485284206a7704e190f
Punit-7 <punit251299@gmail.com>
2025-01-01 22:13:43 +0530

    Data Ingestion and Validation Pipeline

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from src.datascience import logger
+from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 
-logger.info("Welcome to our cusom logging.")
+STAGE_NAME = 'Data Validation Stage'
+
+try:
+    logger.info(f">>>>> Stage {STAGE_NAME} started <<<<<<")
+    onj = DataValidationTrainingPipeline()
+    onj.initiate_data_validation()
+    logger.info(
+        f">>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
